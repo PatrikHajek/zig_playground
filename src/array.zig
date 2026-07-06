1bddcb9c057958479274da17e6f0a943f6d73b4a
PatrikHajek <hajan21@seznam.cz>
2026-07-06 23:02:20 +0200

    refactor: move individual snippets into different modules

---
+++ src/array.zig
@@ -0,0 +1,10 @@
+const std = @import("std");
+
+pub fn array() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const x = try allocator.alloc(i32, 8);
+    std.debug.print("{}", .{x.len});
+}
