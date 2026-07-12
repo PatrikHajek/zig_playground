22d60c1ab6e1eeedbd2765c2fd7ef0cefd085e98
PatrikHajek <hajan21@seznam.cz>
2026-07-12 12:47:36 +0200

    refactor: semantically improve condition

--- src/flip7.zig
+++ src/flip7.zig
@@ -47,7 +47,7 @@ fn Array(comptime T: type, comptime size: u64) type {
         }
 
         fn add(self: *Self, v: T) error{OutOfMemory}!void {
-            if (self.count != size) {
+            if (self.count < size) {
                 self.buffer[self.count] = v;
                 self.count += 1;
             } else {
